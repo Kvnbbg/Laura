e23127843ac87a935d95b4da1d67178d1e48ed03
Kevin Marville <116537485+Kvnbbg@users.noreply.github.com>
2026-01-03 23:18:21 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
+          vendor: ['vite', 'react', 'react-dom', 'react-router-dom'],
         },
       },
     },
