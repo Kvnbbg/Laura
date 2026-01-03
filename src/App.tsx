7be56dad2efb259c4e32c0bfe01e897e5098a760
Kevin Marville <116537485+Kvnbbg@users.noreply.github.com>
2026-01-03 23:21:07 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/commercial" element={<commercial/>
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Layout>
